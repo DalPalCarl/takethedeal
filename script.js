6df71143e4e684054849a980da3f7e6ad72be6b2
Dallas <dallas.carlson10@gmail.com>
2025-04-06 13:01:25 -0500

    Add jsconfig.json to enable JQuery intellisense Add basic form logic for game setup screen Add layout styling for game setup screen

--- script.js
+++ script.js
@@ -3,6 +3,26 @@
 const valueBoard = $("#values");
 const caseBoard = $("#caseContainer");
 const modifiers = $("#modifiers");
+const gameSetupForm = $("#gameSetupForm");
+const playerSetupList = $("#playerSetupList");
+const newPlayerName = $("#newPlayerName");
+
+$("#addPlayer").on("click", () => {
+    if(newPlayerName[0].value !== ""){
+        const newName = newPlayerName[0].value;
+        const newPlayerElement = document.createElement("li");
+        $(newPlayerElement).text(newName).addClass("setupPlayer");
+        playerSetupList.append(newPlayerElement);
+        newPlayerName[0].value = "";
+    }
+})
+
+gameSetupForm.on("submit", (event) => {
+    event.preventDefault();
+    $("#gameSetupBackdrop").fadeOut()
+    // console.log(gameSetupForm[0].playerCount.value);
+    // console.log(gameSetupForm[0].modifierSetting.value);
+})
 
 
 
@@ -13,3 +33,5 @@ function initializeGame() {
 function shuffleValues() {
 
 }
+
+// Animation Functions
