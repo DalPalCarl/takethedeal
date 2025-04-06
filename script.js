cf8f2daf35847426626b5b1dde510543713028c0
Dallas <dallas.carlson10@gmail.com>
2025-04-06 17:57:10 -0500

    Correct Add Player field to add a player with the Enter key, disable Start Game on Enter key

--- script.js
+++ script.js
@@ -6,27 +6,70 @@ const modifiers = $("#modifiers");
 const gameSetupForm = $("#gameSetupForm");
 const playerSetupList = $("#playerSetupList");
 const newPlayerName = $("#newPlayerName");
+const MAXPLAYERCOUNT = 10;
+const ROUNDS = 8;
+
+let playerCount = 0;
+let modifierCount = 0;
 
 $("#addPlayer").on("click", () => {
-    if(newPlayerName[0].value !== ""){
+    addPlayerToList();
+})
+
+$("#newPlayerName").on("keypress", (event) => {
+    if(event.key === "Enter"){
+        event.preventDefault();
+        addPlayerToList();
+    }
+})
+
+gameSetupForm.on("submit", (event) => {
+    event.preventDefault();
+    if(playerSetupList[0].children.length <= 1){
+        alert("Must have at least 2 players to start!");
+    }
+    else {
+        const modifierSetting = gameSetupForm[0].modifierSetting.value
+        playerCount = playerSetupList[0].children.length;
+        modifierCount = modifierSetting === 'low' ? 2 : modifierSetting === 'medium' ? (playerCount * (ROUNDS / 4)) : (playerCount * (ROUNDS / 2));
+        $("#gameSetupBackdrop").fadeOut()
+        initializeGame();
+        //console.log("Players: ", playerCount, " : Modifiers: ", modifierCount);
+
+    }
+})
+
+
+function addPlayerToList() {
+    if(playerSetupList[0].children.length > MAXPLAYERCOUNT){
+        alert("Cannot add any more players!");
+    }
+    else if(newPlayerName[0].value !== ""){
         const newName = newPlayerName[0].value;
         const newPlayerElement = document.createElement("li");
         $(newPlayerElement).text(newName).addClass("setupPlayer");
         playerSetupList.append(newPlayerElement);
         newPlayerName[0].value = "";
     }
-})
-
-gameSetupForm.on("submit", (event) => {
-    event.preventDefault();
-    $("#gameSetupBackdrop").fadeOut()
-    // console.log(gameSetupForm[0].playerCount.value);
-    // console.log(gameSetupForm[0].modifierSetting.value);
-})
-
-
+}
 
 function initializeGame() {
+    loadValues();
+    loadModifiers();
+    loadCases();
+    loadPlayers();
+}
+
+function loadValues() {
+
+}
+function loadModifiers() {
+
+}
+function loadPlayers() {
+
+}
+function loadCases() {
 
 }
 
